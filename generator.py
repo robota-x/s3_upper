bb32a276bd994296b9fa41bcb15fcd3bdfb6b4f1
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-05-16 21:44:15 +0100

    tweak lambda to use proxy integration

--- generator.py
+++ generator.py
@@ -7,18 +7,18 @@ s3_client = boto3.client("s3")
 def create_presigned_post(bucket_name, object_name):
     return s3_client.generate_presigned_post(
         bucket_name, object_name, ExpiresIn=300
-    ).get("url")
+    )
 
 
 def lambda_handler(event, context):
+    
     print("Received event: " + json.dumps(event, indent=2))
 
-    upload_url = create_presigned_post('TODO', 'test-file.txt')
-
-
+    upload_url = create_presigned_post('target', 'test-file.txt')
+    
     return {
+        "isBase64Encoded": False,
         "statusCode": "200",
-        "body": upload_url,
+        "body": json.dumps(upload_url),
         "headers": {"Content-Type": "text/plain"},
     }
-
