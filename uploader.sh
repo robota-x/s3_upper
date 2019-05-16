1906f6286fe9afecd5850b121ba65c4a35c195b3
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-05-16 22:42:37 +0100

    bash the script together

--- uploader.sh
+++ uploader.sh
@@ -1,15 +1,17 @@
 source secrets.sh
 
 get_upload_url() {
-    curl "$APIURL?filename=$1&bucketname=$BUCKETNAME"
+    curl "$APIURL?filename=$1&bucketname=$BUCKETNAME" 2>/dev/null
 }
+export -f get_upload_url
 
 upload_file() {
     curl -X PUT -T $1 -L $2
 }
 
-
-a=$(get_upload_url testfile.jpg)
-
-echo done
-echo $a
+for file in $(ls -p | grep -v /)
+do
+    echo uploading $file .......
+    upload_url=$(get_upload_url $file)
+    upload_file $file $upload_url
+done
